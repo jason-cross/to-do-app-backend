4169e46e15a3697a01174694d9d7f9d738f300a8
Jason Cross <jason.cross1@student.curtin.edu.au>
2023-01-03 17:26:46 +0800

    Database and controller configuration

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -3,31 +3,46 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Todo;
 
 class TodoController extends Controller
 {
     //GET {{base_url}}/todos
     public function index() {
-        return 'displays all';
+        return Todo::all();
     }
 
     //GET {{base_url}}/todo/:id
-    public function create() {
-        return 'displays single todo';
+    public function create(Todo $id) {
+        return $id;
     }
 
     //POST {{base_url}}/todo
     public function store() {
-        return 'saves to database';
+        return Todo::create([
+            'name' => request('name'),
+            'description' => request('description'),
+            'due_date' => request('due_date'),
+            'is_complete' => request('is_complete')
+        ]);
     }
 
     //PATCH {{base_url}}/todo/:id
-    public function update() {
-        return 'updates todo';
+    public function update(Todo $id) {
+        $id->update([
+            'name' => request('name'),
+            'description' => request('description'),
+            'due_date' => request('due_date'),
+            'is_complete' => request('is_complete')
+        ]);
+
+        return $id;
     }
 
     //DELETE {{base_url}}/todo/:id
-    public function destroy() {
-        return 'removes from database';
+    public function destroy(Todo $id) {
+        $id->delete();
+        
+        return $id;
     }
 }
